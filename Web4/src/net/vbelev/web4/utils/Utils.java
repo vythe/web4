c1e6ba2f3f6ef1972410623c637071550fc12d75
Vythe <Vythe@Fateless-7>
2020-01-01 14:27:20 +1100

    utils

---
+++ Web4/src/net/vbelev/web4/utils/Utils.java
@@ -0,0 +1,99 @@
+package net.vbelev.web4.utils;
+
+import java.nio.charset.Charset;
+
+import com.sun.media.jfxmedia.track.Track.Encoding;
+
+public class Utils
+{
+	public static Integer tryParseInt(String arg)
+	{
+		try
+		{
+			return Integer.parseInt(arg);
+		}
+		catch (Exception x)
+		{
+		}
+		return null;
+	}
+
+	public static <T extends Object>T NVL(T... vals)
+	{
+		for (T v : vals)
+		{
+			if (v != null) return v;
+		}
+		return null;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T>T AsClass(Object o, Class<T> c)
+	{
+		if (o == null || !c.isAssignableFrom(o.getClass()))
+			return null;
+		return (T)o;
+		
+	}
+	
+	public static boolean IsEmpty(String arg)
+	{
+		if (arg == null || arg.length() == 0) return true;
+		return arg.trim().length() == 0;
+	}
+	
+	public static String encodeHTML(String src)
+	{
+		if (src == null || src.length() == 0) return "";
+		
+		StringBuilder out = new StringBuilder();
+		char[] chars = src.toCharArray();
+	    for (int i = 0; i < chars.length; i++) {
+	        char c = chars[i];
+	        switch (c)
+	        {
+	        	case '<': out.append("&lt;"); break;
+	        	case '>': out.append("&gt;"); break;
+	        	case '&': out.append("&amp;"); break;
+	        	default:
+	        		if (c > 127)
+	        		{
+	        			out.append("&#" + (int)c + ";");
+	        		}
+	        		else
+	        		{
+	        			out.append(c);
+	        		}
+	        }	       
+	    }
+	    return out.toString();
+	}
+	public static String encodeHTMLAttr(String src)
+	{
+		if (src == null || src.length() == 0) return "";
+		
+		StringBuilder out = new StringBuilder();
+		char[] chars = src.toCharArray();
+	    for (int i = 0; i < chars.length; i++) {
+	        char c = chars[i];
+	        switch (c)
+	        {
+	        	case '<': out.append("&lt;"); break;
+	        	case '>': out.append("&gt;"); break;
+	        	case '&': out.append("&amp;"); break;
+	        	case '"': out.append("&quot;"); break;
+	        	case '\'': out.append("&apos;"); break;
+	        	default:
+	        		if (c > 127 || c < 32)
+	        		{
+	        			out.append("&#" + (int)c + ";");
+	        		}
+	        		else
+	        		{
+	        			out.append(c);
+	        		}
+	        }	       
+	    }
+	    return out.toString();
+	}
+}
---
+++ Web4/src/net/vbelev/web4/utils/XMLiser.java
@@ -0,0 +1,337 @@
+package net.vbelev.web4.utils;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.*;
+import java.util.ArrayList;
+
+import org.w3c.dom.*;
+
+import javax.xml.bind.*;
+import javax.xml.bind.annotation.*;
+//import javax.xml.bind.annotation.*;
+import javax.xml.namespace.QName;
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.*;
+
+
+public class XMLiser
+{
+	@XmlRootElement(name = "object_wrap")
+	public static class ObjectWrapper
+	{
+		@XmlAttribute(name = "class_name")
+		public String className;
+		@XmlElement(name = "content")
+		public String contentString;
+		
+		public ObjectWrapper()
+		{
+			className = null;
+			contentString = null;
+		}
+		
+		public ObjectWrapper(Serializable s)
+		{
+			setObject(s);
+		}
+		public Object getObject()
+		{
+			if (className == null || contentString == null)
+			{
+				return null;
+			}
+			else if (className.equals(String.class.getName()))
+			{
+				return contentString;
+			}
+			else
+			{
+				byte[] objBytes = stringToBytes(contentString);
+				try(ByteArrayInputStream bs = new ByteArrayInputStream(objBytes))
+				{
+				ObjectInputStream is = new ObjectInputStream(bs);
+				Object res = is.readObject();
+				is.close();
+				return res;
+				}
+				catch(ClassNotFoundException x)
+				{
+					throw new IllegalArgumentException("ObjectWrapper.getObject failed", x);
+				}
+				catch(IOException x)
+				{
+					throw new IllegalArgumentException("ObjectWrapper.getObject failed", x);
+				}
+			}
+				
+		}
+		public void setObject(Serializable s)
+		{
+			if (s == null)
+			{
+				className = null;
+				contentString = null;
+			}
+			else if (s instanceof String)
+			{
+				className = s.getClass().getName();
+				contentString = (String)s;
+			}
+			else
+			{
+				className = s.getClass().getName();
+				try(ByteArrayOutputStream bs = new ByteArrayOutputStream())
+				{
+					ObjectOutputStream os = new ObjectOutputStream(bs);
+					os.writeObject(s);
+					os.flush();
+					
+					contentString = stringFromBytes(bs.toByteArray());
+				}
+				catch (IOException x)
+				{
+					throw new IllegalArgumentException("ObjectWrapper.setObject failed", x);
+				}
+				finally
+				{
+				}
+			}
+		}
+	}
+
+	public static final Charset XMLiserCharset = StandardCharsets.UTF_8;
+	protected static final CharsetDecoder XMLiserCharsetDecoder = XMLiserCharset.newDecoder();
+	protected static final CharsetEncoder XMLiserCharsetEncoder = XMLiserCharset.newEncoder();
+	
+	public final Class[] Classes;
+	public final JAXBContext Context;
+	protected Marshaller contextMarshaller;
+	protected Unmarshaller contextUnmarshaller;
+	
+	public static String stringFromBytes(byte[] bytes)
+	{
+		if (bytes == null) return null;
+		if (bytes.length == 0) return "";
+		try
+		{
+			return XMLiserCharsetDecoder.decode(
+					ByteBuffer.wrap(
+							bytes
+					)
+			).toString();
+		}
+		catch (Exception x)
+		{
+			throw new IllegalArgumentException("Failed to convert bytes to " + XMLiserCharset.toString());
+		}
+	}
+	
+	public static byte[] stringToBytes(String s)
+	{
+		if (s == null) return null;
+		if (s.length() == 0) return new byte[0];
+		try
+		{
+			return XMLiserCharsetEncoder.encode(
+					CharBuffer.wrap(
+							s.toCharArray()
+					)
+			).array();
+		}
+		catch (Exception x)
+		{
+			throw new IllegalArgumentException("Failed to convert bytes to " + XMLiserCharset.toString());
+		}
+	}
+
+	public XMLiser()
+	{
+		try
+		{
+			Classes = new Class[0];
+			Context = JAXBContext.newInstance(ObjectWrapper.class);
+			contextMarshaller = Context.createMarshaller();
+			contextMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			
+			contextUnmarshaller = Context.createUnmarshaller();
+		}
+		catch  (Exception x)
+		{
+			throw new IllegalArgumentException("failed to initialize JAXB", x);
+		}
+	}
+	
+	public XMLiser(Class... classesToBeBound)
+	{
+		try
+		{
+			ArrayList<Class> classes = new ArrayList<Class>(classesToBeBound.length + 1);
+			classes.add(ObjectWrapper.class);
+			for (Class c : classesToBeBound)
+			{
+				classes.add(c);
+			}
+			Classes = classesToBeBound.clone();
+			Context = JAXBContext.newInstance(Classes, null);
+			contextMarshaller = Context.createMarshaller();
+			contextMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			contextUnmarshaller = Context.createUnmarshaller();
+		}
+		catch  (Exception x)
+		{
+			throw new IllegalArgumentException("failed to initialize JAXB", x);
+		}
+	}
+	
+	public void toXML(OutputStream out, Object o)
+	{
+		try
+		{
+			if (o == null)
+			{
+				throw new IllegalArgumentException("argument is null");
+				/* can't marshall nulls?
+				ObjectWrapper ow = new ObjectWrapper();
+				contextMarshaller.marshal(ow, out);
+				return;
+				*/
+			}
+			
+			for (Class c : Classes)
+			{
+				if (c.isInstance(o))
+				{
+					contextMarshaller.marshal(o, out);
+					return;
+				}
+			}
+			
+			if (o instanceof Serializable)
+			{
+				ObjectWrapper ow = new ObjectWrapper((Serializable)o);
+				contextMarshaller.marshal(ow, out);
+				return;
+				
+			}
+		}
+		catch  (Exception x)
+		{
+			throw new IllegalArgumentException("failed to serialize object to XML", x);
+		}
+	}
+	
+	public <T>void toXML(OutputStream out, T o, String nodeName)
+	{
+		if (nodeName == null || nodeName.length() == 0)
+		{
+			toXML(out, o);
+			return;
+		}
+		try
+		{
+			if (o == null)
+			{
+				throw new IllegalArgumentException("argument is null");
+				/* can't marshall nulls?
+				ObjectWrapper oo = new ObjectWrapper();
+				@SuppressWarnings("unchecked")
+				JAXBElement<ObjectWrapper> elem = new JAXBElement<ObjectWrapper>( new QName(null, nodeName), ObjectWrapper.class, oo);
+				contextMarshaller.marshal(elem, out);
+				*/
+			}
+			else
+			{
+				@SuppressWarnings("unchecked")
+				JAXBElement<T> elem = new JAXBElement<T>( new QName(null, nodeName), (Class<T>)o.getClass(), o);
+				contextMarshaller.marshal(elem, out);
+			}
+		}
+		catch (Exception x)
+		{
+			throw new IllegalArgumentException("failed to serialize object to XML with node " + nodeName, x);
+		}
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public <T>T fromXML(Class<T> elementClass, InputStream xml)
+	{
+		if (xml == null) return null;
+		
+		//lock.lock();
+		try
+		{
+		//StringReader tReader = new StringReader(xml);
+
+			StreamSource ss = new StreamSource( xml);
+		
+			for (Class c : Classes)
+			{
+				if (elementClass.isAssignableFrom(c))
+				{
+					return contextUnmarshaller.unmarshal(ss, elementClass).getValue();
+				}
+			}
+			ObjectWrapper ow = contextUnmarshaller.unmarshal(ss, ObjectWrapper.class).getValue();
+			
+			Object o = ow.getObject();
+			if (o == null)
+			{
+				return null;
+			}
+			return (T)o; // this will throw a class cast exception
+		
+		}
+		catch (Exception x)
+		{
+			throw new IllegalArgumentException("Unmarshaller", x);
+		}
+		finally
+		{
+			//lock.unlock();
+		}
+	}
+	
+	public Document streamToDocument(InputStream is)
+	{
+		try
+		{
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder docBuilder = dbf.newDocumentBuilder();
+	        
+	        return docBuilder.parse(is);
+		}
+		catch (Exception x)
+		{
+			throw new IllegalArgumentException("streamToDocument", x);
+		}
+	}
+	
+	public OutputStream streamFromDocument(Document doc, OutputStream os)
+	{
+		try
+		{
+		//DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        //DocumentBuilder docBuilder = dbf.newDocumentBuilder();
+
+			if (os == null)
+			{
+				os = new ByteArrayOutputStream();
+			}
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer docTransformer = tf.newTransformer();
+        docTransformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        docTransformer.transform(new DOMSource(doc.getDocumentElement()), new StreamResult(os) );
+        
+        return os;
+		}
+		catch (Exception x)
+		{
+			throw new IllegalArgumentException("streamToDocument", x);
+		}
+	}
+}
