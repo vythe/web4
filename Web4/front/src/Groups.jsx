2d47c560c616889152470a5fc46ccbc7ed930d03
Vythe <Vythe@Fateless-7>
2020-01-07 13:15:18 +1100

    07/01 13:15 from fateless

--- Web4/front/src/Groups.jsx
+++ Web4/front/src/Groups.jsx
@@ -2,6 +2,53 @@ import React from 'react';
 import axios from 'axios';
 import {Utils} from './Utils'
 
+class AffinityCell extends React.Component {
+
+  constructor(props) {
+    super(props);
+    // props.moniker
+    // props.aff = {toMoniker, quality, value}
+    // props.onClick = function(moniker, toMoniker, value, quality) from the parent
+    this.state = {
+      aff: props.aff,
+      onClick: props.onClick
+    };
+    this.cellClickHandler = this.cellClickHandler.bind(this);    
+  }
+
+  cellClickHandler() {
+    alert(JSON.stringify({moniker: this.props.moniker, ...this.props.aff}))
+  }
+  render() {
+    let aff = this.state.aff;
+
+    let valColour = "inherit";
+    let text = "";
+    let title = "";
+    if (!aff || aff.quality == "NONE") {
+      text = "---";
+      title = "Not defined";
+    } else if (aff.quality == "CALCULATED") {
+      valColour = "#20" + (aff.value * 255).toString(16).padStart(2, "0").substring(0, 2) + "20";
+      text = aff.value;
+      title = "Calculated";
+    } else {
+      let valColour = "#" 
+      + (aff.value * 255).toString(16).padStart(2, "0").substring(0, 2)
+      + (aff.value * 255).toString(16).padStart(2, "0").substring(0, 2) 
+      + "20";
+      text = aff.value;
+      title = "Assigned";
+    }
+    return (
+      <td toMoniker={aff.toMoniker} title={title} style={{backgroundColor: valColour}} onClick={this.cellClickHandler}>{text}</td>
+    );
+    
+  }
+
+}
+
+//========================================================================
 export class Groups extends React.Component {
 
 
@@ -16,12 +63,17 @@ export class Groups extends React.Component {
     };
     // declarations: member functions
     this.load = this.load.bind(this);
+    this.reloadClickHandle = this.reloadClickHandle.bind(this);
+  }
+
+  componentDidMount() {
+    this.load();
   }
 
   async load() {
-    var app = this;
-    console.log("Groups.load called");
     /*
+    console.log("Groups.load called");
+    var app = this;
     fetch(window.appconfig.apiurl + "get_groups")
     .then(function(res) { return res.json(); })
     .then((res) => {
@@ -41,24 +93,27 @@ export class Groups extends React.Component {
     this.setState({loadCount: this.state.loadCount + 1});
     */
     axios({
-  method: 'get',
-  url: window.appconfig.apiurl + "get_groups"
-  //, timeout: 4000,    // 4 seconds timeout
-  //, data: {firstName: 'David',lastName: 'Pollock'}
-})
-.then(res => {
-/* handle the response */
-app.data = res;
-this.data = res;
-console.log("axios received: " + JSON.stringify(res));
-this.setState({loadCount: this.state.loadCount + 1});
-})
-.catch(error => console.error('axios error: ' + error))
+      method: 'get',
+      url: window.appconfig.apiurl + "get_groups"
+      //, timeout: 4000,    // 4 seconds timeout
+      //, data: {firstName: 'David',lastName: 'Pollock'}
+    })
+    .then(res => {
+    /* handle the response */
+    //app.data = res.data;
+    this.data = res.data;
+    console.log("axios received: " + JSON.stringify(res));
+    this.setState({loadCount: this.state.loadCount + 1});
+    })
+    .catch(error => console.error('axios error: ' + error))
   }
 
-  componentDidMount() {
+  reloadClickHandle() {
     this.load();
-    console.log("didMount final: " + JSON.stringify(this.data));
+  }
+
+  affinityClickHandle() {
+    alert("clicked " + JSON.stringify(this));
   }
 
   render() {
@@ -68,11 +123,61 @@ this.setState({loadCount: this.state.loadCount + 1});
         <div className="notification">No data</div>
       )
     } else {
+      let groupCount = this.data.length;
+      /* the data structure is [group... ]
+      group: {moniker, name, [affinity...]}
+      affinity: {toMoniker, quality, value}
+      */
+
+      let headerCells = [];
+      let monikers = [];
+      for (var k in this.data) {
+        monikers.push(this.data[k].moniker);
+        headerCells.push(
+          <th key={"header_" + this.data[k].moniker}>{this.data[k].name}</th>
+        );
+      }
+
+      
+      let rows = [];
+      for (var k in this.data) {
+        let rowCells = [];
+        let row = this.data[k];
+        for (var m in monikers) {
+          let aff = row.affinities.find(a => a.toMoniker == monikers[m]);
+          rowCells.push(
+          <AffinityCell moniker={row.moniker} aff={aff} onClick={null}/>
+          );
+        }
+        //rows.push(
+        //  <tr key={this.data[k].moniker}><td>{JSON.stringify(this.data[k])}</td></tr>
+        //)
+        rows.push((
+          <tr key={"row_" + row.moniker}>
+            <td>{row.name}</td>
+            {rowCells}
+          </tr>
+        ))
+      }
       return (
+      <div>
+        <div className="notification">LoadCount {this.state.loadCount}
+            <button onClick={this.reloadClickHandle}>Reload</button>
+        </div>
         <div className="notification">{typeof(this.data) + ": " +
         //JSON.stringify(Utils.squash(this.data))
         JSON.stringify(this.data)
       }</div>
+        <table className="info_table">
+          <thead>
+            <tr><th> </th><th colspan={headerCells.length}>Affinity to (total of {rows.length})</th></tr>
+            <tr><th> </th>{headerCells}</tr>
+          </thead>
+          <tbody>
+          {rows}
+          </tbody>
+        </table>
+      </div>
       )
     }
   }
